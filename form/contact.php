3e08575e86fb8c46ad4e724b058f70df5b18ab6c
Saksham Shekher <67940483+Oshekher@users.noreply.github.com>
2021-05-11 13:13:53 +0530

    Update contact.php

--- form/contact.php
+++ form/contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <sender@yourdomain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <reciever@example.com>';
+$sendTo = 'shekhersaksham@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
